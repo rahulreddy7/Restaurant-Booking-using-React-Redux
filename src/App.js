a55ea977ca8565ec43ea6a9b795936121aa4b27d
S.Rahul Reddy <rahulsarikonda@gmail.com>
2019-02-04 13:22:17 +0530

    Added Menu component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import {Navbar, NavbarBrand} from 'reactstrap';
 import './App.css';
+import Menu from './components/MenuComponent';
+import {DISHES} from './shared/dishes';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      dishes:DISHES
+    };
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Navbar dark color="primary">
+          <div className='container'>
+            <NavbarBrand href='/'>Pluto</NavbarBrand>
+          </div>          
+        </Navbar>
+        <Menu dishes={this.state.dishes}/>
       </div>
     );
   }
