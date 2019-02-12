d0b1f2fa013adca6b2e8cd21cf2ebca9c95f8694
S.Rahul Reddy <rahulsarikonda@gmail.com>
2019-02-12 19:39:47 +0530

    single page application

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -35,12 +35,21 @@ class Main extends Component {
         leader={this.state.leaders.filter((leader) => leader.featured)[0]} />
       );
     }
+    const DishWithId=({match})=>{
+      return(
+        <Dishdetail dish={this.state.dishes.filter((dish)=>dish.id===parseInt(match.params.dishID),10)[0]}
+          comments={this.state.comments.filter((comment)=>comment.dishId===parseInt(match.params.dishID),10)}
+          />
+      );
+
+    }
     return (
       <div className="App">
         <Header />
         <Switch>
         <Route path='/home' component={HomePage} />
         <Route exact path='/menu' component={()=><Menu dishes={this.state.dishes} />}/>
+        <Route path='/menu/:dishID' component={DishWithId}/>
         <Route exact path='/contactus' component={Contact} />
         <Redirect to='/home' />
          </Switch>
